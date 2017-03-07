f7c1775c40cb2582a0d24be2f614e325132ab49b
jshallow <joshuashallow@gmail.com>
2017-03-07 14:25:17 -0800

    first commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+
+    private bool fishDropped;
+    
+	void Start () {
+        fishDropped = false;
+
+	}
+	
+	void Update () {
+
+        if(!fishDropped) {
+            // drop fish
+        }
+	}
+}
