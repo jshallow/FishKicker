df7f8002933886e4938fa24211f121dfe22497d6
nosnhojttam <matjohnson@csumb.edu>
2017-03-07 16:23:34 -0800

    added kicking

---
+++ Assets/Scripts/LegController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegController : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			//Quaternion 
+			rb.AddTorque (transform.forward * 1000000);
+		}
+	}
+}
